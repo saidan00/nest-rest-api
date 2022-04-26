4666cb975e7cb9ce0b828f70b96fc27c28a134c9
Huy Vo <huy.vo@kyanon.digital>
2022-04-26 10:06:33 +0700

    REST functions

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,17 +1,30 @@
-import { Controller, Get, Post, Put, Delete, Body, Req, Res } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
-import { Request, Response } from 'express';
 
 @Controller('items')
 export class ItemsController {
   @Get()
-  findAll(@Req() req: Request, @Res() res: Response): Response {
-    console.log(req.url);
-    return res.send('Hello World');
+  findAll(): string {
+    return 'Get all items';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id): string {
+    return `Item ${id}`;
   }
 
   @Post()
   create(@Body() createItemDto: CreateItemDto): string {
     return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete ${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Update item ${id} Name ${updateItemDto.name}`;
   }
 }
