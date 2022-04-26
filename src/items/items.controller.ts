6021e4fbf685e730ad0cd3f17838f5f7f58ddeb1
Huy Vo <huy.vo@kyanon.digital>
2022-04-26 09:43:40 +0700

    Controller, DTO, req res

---
+++ src/items/items.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Put, Delete, Body, Req, Res } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { Request, Response } from 'express';
+
+@Controller('items')
+export class ItemsController {
+  @Get()
+  findAll(@Req() req: Request, @Res() res: Response): Response {
+    console.log(req.url);
+    return res.send('Hello World');
+  }
+
+  @Post()
+  create(@Body() createItemDto: CreateItemDto): string {
+    return `Name: ${createItemDto.name} Desc: ${createItemDto.description}`;
+  }
+}
